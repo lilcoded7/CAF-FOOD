8cc5aeb892b4776ff9623f72cb26b07d2ed53722
coded <coded@gmail.com>
2023-09-20 10:22:45 +0000

    updated

--- setup/urls.py
+++ setup/urls.py
@@ -5,22 +5,6 @@ from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static 
 
-# third part settings 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        #  add your swagger doc title
-        title="CAF-FOOD",
-        #  version of the swagger doc
-        default_version='v1',
-        # first line that appears on the top of the doc
-        description="CAF-FOOD",
-    ),
-    public=True,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
